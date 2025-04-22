8bfbbabd039ea2d51df48f24090adfde94ffc555
TheanLim <theanc.lim@gmail.com>
2025-04-22 22:27:04 +0000

    Address comments

--- agent/s3/factory/factory.go
+++ agent/s3/factory/factory.go
@@ -80,20 +80,20 @@ func createAWSConfig(region string, creds credentials.IAMRoleCredentials, useFIP
 			fmt.Fprintln(os.Stdout, args...)
 		}))
 
-	switch {
-	case useFIPSEndpoint && useDualStackEndpoint:
-		logger.Debug("FIPS mode detected, using DualStack FIPS-compliant S3 endpoint in supported regions")
-		cfg.UseFIPSEndpoint = endpoints.FIPSEndpointStateEnabled
+	endpointTypeMsg := ""
+	if useDualStackEndpoint {
 		cfg.UseDualStackEndpoint = endpoints.DualStackEndpointStateEnabled
-	case useFIPSEndpoint:
-		logger.Debug("FIPS mode detected, using FIPS-compliant S3 endpoint in supported regions")
-		cfg.UseFIPSEndpoint = endpoints.FIPSEndpointStateEnabled
-	case useDualStackEndpoint:
-		logger.Debug("Using DualStack S3 endpoint")
-		cfg.UseDualStackEndpoint = endpoints.DualStackEndpointStateEnabled
-	default:
-		logger.Debug("Using default S3 endpoint")
+		endpointTypeMsg += "DualStack "
 	}
+	if useFIPSEndpoint {
+		cfg.UseFIPSEndpoint = endpoints.FIPSEndpointStateEnabled
+		endpointTypeMsg += "FIPS-compliant "
+	}
+
+	if endpointTypeMsg == "" {
+		endpointTypeMsg = "default "
+	}
+	logger.Debug(fmt.Sprintf("Using %sS3 endpoint", endpointTypeMsg))
 
 	return cfg
 }
