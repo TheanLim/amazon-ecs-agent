9717f3b45565e4bcf377c05adfcdaf23af0a7d40
Thean Lim <theanlim@amazon.com>
2025-09-15 18:36:53 -0700

    more pin

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -18,7 +18,7 @@ set -e -x
 
 CURRENTDIR=$(dirname "${0}")
 version=$(cat "${CURRENTDIR}"/../ecs-init/ECSVERSION)
-git_hash=$(git rev-parse --short=8 HEAD)
+git_hash="b96dada0"
 git_dirty=false
 
 if [[ "$(git status --porcelain)" != "" ]]; then
